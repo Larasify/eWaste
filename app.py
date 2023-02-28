73e993b8ce46f92e9002f399b7146a9552bd163d
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-02-28 13:03:40 +0000

    sample mongodb

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import random
 
 app = Flask(__name__)
+#connect to mongo client
+client = MongoClient('localhost ', 27017)
+db = client.flask_db
+test_db = db.test_db
+#app.run(debug=True)
 
 @app.route('/')
 @app.route('/index')
@@ -10,11 +17,14 @@ def index():
 
 @app.route('/apigettest')
 def apigettest():
-    import random
     random_results = [{'id': random.randint(1, 100), 'name': random.choice(['John', 'Jane', 'Bob', 'Alice', 'Joe'])} for i in range(5)]
     return {"results": random_results}
 
 @app.route('/apiposttest', methods=['POST'])
 def apiposttest():
     print(request.json)
+    add_to_database()
     return {"results": request.json}
+
+def add_to_database():
+    test_db.insert_one({'id': random.randint(1, 100), 'name': random.choice(['John', 'Jane', 'Bob', 'Alice', 'Joe'])})
