918b005b49fdc732a72438bc7e027605383a053b
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2023-03-07 12:24:12 +0000

    FRONT_END package: added react-router-dom refactor: redid layout feature: added router. feature: added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import Header from './Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Layout from './fragments/Layout.js';
+import Register from './pages/Register.js';
+import Home from './pages/Home.js';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
