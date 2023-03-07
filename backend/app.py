00e3f0d379848005d3e5bd3e79fe312ee26ebc60
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-03-07 14:37:41 +0000

    uuid-generate and setting cookies

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from payment import payment_api
 from auth import auth_api
 from datasources import datasources_api
 from datalinks import datalinks_api
+import uuid
 
 
 app = Flask(__name__)
@@ -28,12 +29,29 @@ def serve(path):
 
 @app.route('/apigettest')
 def apigettest():
-    import random
-    random_results = [{'id': random.randint(1, 100), 'name': random.choice(['John', 'Jane', 'Bob', 'Alice', 'Joe'])} for i in range(5)]
-    return {"results": random_results}
+    print(request.json)
+    if('session-id' in request.cookies):
+        print(request.cookies.get('session-id'))
+        return {"results": request.json}
+    else:
+        generated_session_id = generate_random_session_id()
+        response = app.make_response({"results": request.json})
+        response.set_cookie('session-id', generated_session_id)
+        return response
+
 
 @app.route('/apiposttest', methods=['POST'])
 def apiposttest():
     print(request.json)
-    return {"results": request.json}
+    if('session-id' in request.cookies):
+        print(request.cookies.get('session-id'))
+        return {"results": request.json}
+    else:
+        generated_session_id = generate_random_session_id()
+        response = app.make_response({"results": request.json})
+        response.set_cookie('session-id', generated_session_id)
+        return response
+
+def generate_random_session_id():
+    return str(uuid.uuid4())
 
