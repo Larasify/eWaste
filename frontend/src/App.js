2611b0f2c3a9b6891bd0dda493e95f719034a2c6
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2023-03-07 10:44:34 +0000

    - added header to layout (non-functional)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,12 @@
-import logo from './logo.svg';
+import Header from './Header';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
     </div>
-  );
+  )
 }
-async function getBotResponse() {
-  const jsondata = {"message": "hello"};
-  const options = {
-      method: 'POST',
-      credentials: "same-origin",
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(jsondata)
-  };
-  const response = await fetch('http://127.0.0.1:5000/apiposttest', options);
-  const responsedata = await response.json();
-  console.log(responsedata);
-}
-getBotResponse();
 
 export default App;
---
+++ frontend/src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css'
+import { BsSearch } from 'react-icons/bs';
+import { RiUser5Fill } from 'react-icons/ri';
+import { RiNotification3Line } from 'react-icons/ri';
+import { RiListSettingsLine } from 'react-icons/ri';
+
+export default function Header() {
+  return (
+    <div class="head-holder">
+      <div class="head" style={{
+        backgroundImage: `url('/header.png')`
+      }}>
+          <div class="nav-search">
+            <div class="search-box">
+              <input type="text" placeholder='Search'></input>
+              <span><BsSearch/></span>
+            </div>
+          </div>
+          <div class="recycle-btn">
+            <span class="btn">
+              Recycle
+            </span>
+          </div>
+          <div class="right-section">
+            <div class="account">
+              <RiUser5Fill/>
+            </div>
+            <div class="notifications">
+              <RiNotification3Line/>
+            </div>
+            <div class="options">
+              <RiListSettingsLine/>
+            </div>
+          </div>
+      </div>
+    </div>
+
+  )
+}
