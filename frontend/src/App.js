cca9c1cc09b4eb361bf2a2115978eb905a782d9e
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2023-03-12 19:57:00 +0000

    package: added ant-design library app: registered device endpoint header: 	fix: fixed buttons. they are more button-ey now 	fix: fixed styled and tweaked some classes 	feat: added account name 	fix: removed recycle button modal: fixed classes bug due to conflict device: feat: added device page home: refactor: re-did home with more features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './fragments/Login.js';
 import Layout from './fragments/Layout.js';
 import Register from './pages/Register.js';
+import Device from './pages/Device.js';
 import Home from './pages/Home.js';
 import './App.css';
 
@@ -14,6 +15,7 @@ const App = () => {
           <Route index element={<Home/>}/>
           <Route path="login" element={<Login/>}/>
           <Route path="register" element={<Register/>}/>
+          <Route path="device" element={<Device/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
