22c0af66e4347929afef7bc4b1229c54b8b282fe
Hongyu Pan <Hpan21@sheffield.ac.uk>
2023-03-14 16:07:48 +0000

    rough payment page version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Layout from './fragments/Layout.js';
 import Register from './pages/Register.js';
 import Device from './pages/Device.js';
 import Home from './pages/Home.js';
+import Payment from "./pages/Payment";
+import PaySuc from "./pages/PaySuc";
 import './App.css';
 
 const App = () => {
@@ -16,6 +18,8 @@ const App = () => {
           <Route path="login" element={<Login/>}/>
           <Route path="register" element={<Register/>}/>
           <Route path="device" element={<Device/>}/>
+          <Route path="payment" element={<Payment/>}/>
+          <Route path="paysuc" element={<PaySuc/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
