ca0dfdb2a3f6fb2a6fe1d4cbef3e42f3a580ecb4
Ivan Tse <sdyaivan@icloud.com>
2023-03-15 18:41:08 +0000

    Responsive navbar done

---
+++ frontend/src/pages/editUserForm.js
@@ -0,0 +1,13 @@
+import './editUserForm.css';
+import React from "react";
+import logo from "../images/logo.png"
+import {RiNotification3Line, RiSearchLine, RiUser5Fill} from "react-icons/ri";
+
+export default function EditUserForm() {
+    return (
+        <div>
+            hello world
+        </div>
+
+    )
+}
