6893a0fb7d46091dde745eb98f2ed9be96fd5331
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-03-18 15:06:52 +0000

    functional auth, account and user api's

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory, request
 from flask_cors import CORS
+import uuid
 from pymongo import MongoClient
 myclient = MongoClient("mongodb://localhost:27017/")
 db = myclient["ewaste_db"]
+session_ids = dict()
 from user import user_api, account_api
 from device import device_api
 from payment import payment_api
@@ -23,6 +25,7 @@ app.register_blueprint(datalinks_api, url_prefix='/datalinks')
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
+
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
@@ -30,13 +33,22 @@ def serve(path):
 
 @app.route('/apigettest')
 def apigettest():
-    import random
-    random_results = [{'id': random.randint(1, 100), 'name': random.choice(['John', 'Jane', 'Bob', 'Alice', 'Joe'])} for i in range(5)]
-    return {"results": random_results}
+    print(request.json)
+    if('session-id' in request.cookies and request.cookies.get('session-id') in session_ids):
+        print(request.cookies.get('session-id'))
+        return {"response":"success"}
+    else:
+        generated_session_id = str(uuid.uuid4())
+        response = app.make_response({"response":"success", "message":"cookie generated"})
+        response.set_cookie('session-id', generated_session_id)
+        session_ids[generated_session_id] = str(uuid.uuid4())
+        print(session_ids)
+        return response
+
 
 @app.route('/apiposttest', methods=['POST'])
 def apiposttest():
     print(request.json)
     return {"results": request.json}
 
-rebuilddb()
+#rebuilddb()
--- backend/user.py
+++ backend/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app import db
+from app import db, session_ids
 from bson.objectid import ObjectId
 from bson.json_util import dumps
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -20,8 +20,8 @@ def getUser():
 @user_api.route("/getuserlist")
 def getUserList():
     users = db.Users.find()
-    if len(users) == 0:
-        return {"message":"empty list"}
+    if users.count() == 0:
+        return {"message":"list_empty", "response":"error"}
     list_users = list(users)
     json_users = dumps(list_users)
     return json_users
@@ -30,7 +30,7 @@ def getUserList():
 def postUser():
     userid = str(uuid.uuid4())
     data = request.get_json()
-    email = data.get("email")
+    email = data.get("email") #TODO: What is the difference between this and register
     password = generate_password_hash(data.get("password"))
     first_name = data.get("first_name")
     ts = datetime.utcnow()
@@ -38,9 +38,9 @@ def postUser():
     if "last_name" in data:
         last_name = data.get("last_name")
         db.Users.insert_one({"_id":userid,"email":email, "password":password, "first_name":first_name,"last_name":last_name,"ts":ts,"ts_mod":ts_mod})
-        return {"message":"success"}
+        return {"response":"success"}
     db.Users.insert_one({"_id":userid,"email":email, "password":password, "first_name":first_name,"ts":ts,"ts_mod":ts_mod})
-    return {"message":"success"}
+    return {"response":"success"}
 
 @user_api.route("/deleteuser", methods=['POST'])
 def deleteUser():
@@ -50,9 +50,9 @@ def deleteUser():
     newvalues = { "$set": { "ts_mod": datetime.utcnow(),"is_deleted":True}}
     result = db.Users.update_one(query, newvalues)
     if result.matched_count == 1:
-        return {"message": "user deleted successfully"}
+        return {"response":"success"}
     else:
-        return {"message": "user does not exist"}
+        return {"message": "user does not exist", "response":"error"}
 
 @user_api.route("/updateuser", methods=['POST'])
 def updateUser():
@@ -67,38 +67,45 @@ def updateUser():
     update_dict["ts_mod"] = datetime.utcnow()
     result = db.Users.update_one(query, {"$set": update_dict})
     if result.matched_count == 1:
-        return {"message": "user updated successfully"}
+        return {"response":"success"}
     else:
-        return {"message": "user does not exist"}
+        return {"message": "user does not exist", "response":"error"}
 
 account_api = Blueprint('account_api', __name__)
 
 @account_api.route("/getuserlistings", methods=['POST'])
 def getUserListings():
-    data = request.get_json()
-    userid = data.get("id")
-    mylist = db.Devices.find({"user_id":userid})
-    if len(mylist) == 0:
-        return {"message":"empty list"}
-    json_list = dumps(list(mylist))
-    return json_list
+    if('session-id' in request.cookies and request.cookies.get('session-id') in session_ids):
+        userid = session_ids[request.cookies.get('session-id')]
+        mylist = db.Listings.find({"user_id":userid})
+        if len(mylist) == 0:
+            return {"message":"empty list"}
+        json_list = dumps(list(mylist))
+        return json_list
+    else:
+        return {"message":"not_logged_in", "response":"error"}
 
 @account_api.route("/getuserpayments", methods=['POST'])
 def getUserPayments():
-    data = request.get_json()
-    userid = data.get("id")
-    mylist = db.Payments.find({"user_id":userid})
-    if len(mylist) == 0:
-        return {"message":"empty list"}
-    json_list = dumps(list(mylist))
-    return json_list
+    if('session-id' in request.cookies and request.cookies.get('session-id') in session_ids):
+        userid = session_ids[request.cookies.get('session-id')]
+        mylist = db.Payments.find({"user_id":userid})
+        if len(mylist) == 0:
+            return {"message":"empty list"}
+        json_list = dumps(list(mylist))
+        return json_list
+    else:
+        return {"message":"not_logged_in", "response":"error"}
 
 @account_api.route("/getuserdatalinks", methods=['POST'])
 def getUserDataLinks():
-    data = request.get_json()
-    userid = data.get("id")
-    mylist = db.Devices.find({"user_id":userid},{"data_retrieval_link":1, "_id":0})
-    if len(mylist) == 0:
-        return {"message":"empty list"}
-    json_list = dumps(list(mylist))
-    return json_list
+    if('session-id' in request.cookies and request.cookies.get('session-id') in session_ids):
+        userid = session_ids[request.cookies.get('session-id')]
+        mylist = db.Devices.find({"user_id":userid},{"data_retrieval_link":1, "_id":0})
+        if len(mylist) == 0:
+            return {"message":"empty list"}
+        json_list = dumps(list(mylist))
+        return json_list
+    else:
+        return {"message":"not_logged_in", "response":"error"}
+
