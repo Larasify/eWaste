30332aaf9c2ebcd0bb7c72460bb50d4a22813980
Zeyi Liu <zeyiliu@Zeyis-MacBook-Pro.local>
2023-03-26 22:09:50 +0100

    new api functions

--- backend/datasources.py
+++ backend/datasources.py
@@ -39,8 +39,17 @@ def getAll():
 #postvendor
 @datasources_api.route("/postvendor", methods=['POST'])
 def postVendor():
-    id = str(uuid.uuid4())
-    #todo
+    vendor_id = str(uuid.uuid4())
+    data = request.get_json()
+    brand = data.get("brand")
+    model_name = data.get("model_name")
+    size = data.get("size")
+    storage = data.get("storage")
+    sell_price = data.get("sell_price")
+    ts = datetime.utcnow()
+    ts_mod = datetime.utcnow()
+    db.Vendors.insert_one({"_id":vendor_id,"brand":brand,"model_name":model_name,"size":size,"storage":storage,\
+                           "sell_price":sell_price,"ts":ts,"ts_mod":ts_mod,"is_deleted":False})
     return {"response":"success"}
 
 #deletevendor
--- backend/device.py
+++ backend/device.py
@@ -24,7 +24,7 @@ def getDevice():
 def getDeviceList():
     devices = db.Devices.find()
     if devices.count() == 0:
-        return {"message":"empty list"}
+        return {"message":"empty list", "response":"error"}
     list_devices = list(devices)
     json_devices = dumps(list_devices)
     return json_devices
@@ -35,32 +35,35 @@ def postDevice():
     device_id = str(uuid.uuid4())
     data = request.get_json()
     user_id = data.get("user_id")
-    cost = data.get("cost")
+    vendor_id = data.get("vendor_id")
+    status = data.get("status")
+    color = data.get("color")
     dtype = data.get("type")
     ts = datetime.utcnow()
     ts_mod = datetime.utcnow()
-    db.Devices.insert_one({"_id":device_id,"user_id":user_id,"cost":cost,"dtype":dtype,"ts":ts,"ts_mod":ts_mod,"is_deleted":False})
-    return {"message":"success"}
+    db.Devices.insert_one({"_id":device_id,"user_id":user_id,"vendor_id":vendor_id,"status":status,"color":color,\
+                           "dtype":dtype,"ts":ts,"ts_mod":ts_mod,"is_deleted":False})
+    return {"response":"success"}
 
 # Delete a device
 @device_api.route("/deletedevice")
 def deleteDevice():
     data = request.get_json()
     device_id = data.get("id")
-    query = {"_id":uuid.UUID(device_id)}
+    query = {"_id":device_id}
     newvalues = { "$set": { "ts_mod": datetime.utcnow(),"is_deleted":True}}
     result = db.Devices.update_one(query, newvalues)
     if result.matched_count == 1:
-        return {"message": "Device deleted successfully"}
+        return {"response": "success"}
     else:
-        return {"message": "Device does not exist"}
+        return {"message": "Device does not exist", "response":"error"}
 
 # Update a device
 @device_api.route("/updatedevice")
 def updateDevice():
     data = request.get_json()
     device_id = data.get("id")
-    query = {"_id": uuid.UUID(device_id)}
+    query = {"_id":device_id}
     fields = data.get("fields")
     values = data.get("values")
     update_dict = {}
@@ -69,8 +72,8 @@ def updateDevice():
     update_dict["ts_mod"] = datetime.utcnow()
     result = db.Devices.update_one(query, {"$set": update_dict})
     if result.matched_count == 1:
-        return {"message": "Device updated successfully"}
+        return {"response": "success"}
     else:
-        return {"message": "Device does not exist"}
+        return {"message": "Device does not exist", "response":"error"}
 
     
