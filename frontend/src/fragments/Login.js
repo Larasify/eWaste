5f4c6095cc458f31ce2c055c109aefe45440cf34
Ivan Tse <sdyaivan@icloud.com>
2023-05-01 21:51:16 +0100

    ...

--- frontend/src/fragments/Login.js
+++ frontend/src/fragments/Login.js
@@ -15,8 +15,8 @@ const loginOnclick = ()=>{
     })
 }
 
-const loginSubmit = (email, password)=>{
-    const myRequest = new Request("http://127.0.0.1:5000/auth/login", {
+export const loginSubmit = (email, password)=>{
+    const myRequest = new Request("http://127.0.0.1:8080/auth/login", {
         headers: new Headers({'Content-Type': 'application/json'}),
         method: "POST",
         body: JSON.stringify({
@@ -37,28 +37,36 @@ export default function Login(props) {
     }
 
     return (
-        <div className={"bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 outline-0 flex flex-col my-auto py-5 px-10 rounded-2xl"}>
+        <div
+            className={"bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 outline-0 flex flex-col my-auto py-5 px-10 rounded-2xl"}>
             <div
                 className={"text-[#237759] text-2xl md:text-4xl lg:text-6xl justify-center font-bold w-full flex p-4"}>
                 <span>Login</span>
             </div>
             <div className={"flex flex-col w-full"}>
-                <label className={"text-left block mb-4 text-xl font-medium text-gray-900 dark:text-white"}>Email
-                    Address</label>
+                <label className={"text-left block mb-4 text-xl font-medium text-gray-900 dark:text-white"} htmlFor={"emailInput"}>
+                    Email Address
+                </label>
                 <input
                     className={"block w-full p-3 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
-                    type="text"></input>
+                    type="text"
+                    id={"emailInput"}
+                ></input>
             </div>
             <div className={"flex flex-col w-full mt-4 lg:mt-6"}>
-                <label
-                    className={"text-left block mb-4 text-xl font-medium text-gray-900 dark:text-white"}>Password</label>
+                <label className={"text-left block mb-4 text-xl font-medium text-gray-900 dark:text-white"} htmlFor={"passwordInput"}>
+                    Password
+                </label>
                 <input
                     className={"block w-full p-3 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
-                    type="password"></input>
+                    type="password" id={"passwordInput"}></input>
             </div>
-            <span className={"text-right my-2 text-sm md:text-md  cursor-pointer underline "}>Forgot your password?</span>
+            <span
+                className={"text-right my-2 text-sm md:text-md  cursor-pointer underline "}>Forgot your password?</span>
             <div
-                className={"mt-4 text-white flex bg-[#509E82] mx-auto h-full justify-center w-4/5 text-xl p-3 items-center font-bold cursor-pointer rounded-2xl hover:bg-[#4E8E77]"}>
+                className={"mt-4 text-white flex bg-[#509E82] mx-auto h-full justify-center w-4/5 text-xl p-3 items-center font-bold cursor-pointer rounded-2xl hover:bg-[#4E8E77]"}
+                onClick={loginOnclick}
+            >
                 <span className="register">SIGN IN</span>
             </div>
             <div
@@ -71,5 +79,5 @@ export default function Login(props) {
                 <div className={"ml-4"}><FcGoogle size={30}/></div>
             </div>
         </div>
-    )
+    );
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -44,43 +44,66 @@ const registerOnclick = () => {
 };
 
 export default function Register() {
-  return (
-    <div class={"flex justify-center items-center bg-gray-50 w-full h-full overflow-auto"}>
-        <div class={"items-center grid grid-col-7 w-full p-8 md:w-1/2 bg-[#f3f9f7] rounded-2xl "}>
-            <span className={"justify-center flex w-full mt-2 md:mt-12 text-[#237759] font-bold text-3xl md:text-4xl lg:text-5xl"}>Create Account</span>
-            <div className={"w-full lg:w-3/5 mx-auto my-auto"}>
-                <div class={"md:grid md:grid-cols-2 gap-x-4 mt-6 mb-3"}>
-                    <div >
-                        <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>* First Name</label>
-                        <input className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"} type="text"></input>
+    return (
+        <div className={"flex justify-center items-center bg-gray-50 w-full h-full overflow-auto"}>
+            <div className={"items-center grid grid-col-7 w-full p-8 md:w-1/2 bg-[#f3f9f7] rounded-2xl "}>
+                <span
+                    className={"justify-center flex w-full mt-2 md:mt-12 text-[#237759] font-bold text-3xl md:text-4xl lg:text-5xl"}>Create Account</span>
+                <div className={"w-full lg:w-3/5 mx-auto my-auto"}>
+                    <div className={"md:grid md:grid-cols-2 gap-x-4 mt-6 mb-3"}>
+                        <div>
+                            <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>*
+                                First Name</label>
+                            <input
+                                className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
+                                type="text"
+                                id={"firstNameInput"}
+                            ></input>
+                        </div>
+                        <br className={"block md:hidden"}/>
+                        <div>
+                            <label
+                                className={" text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>*
+                                Last Name</label>
+                            <input
+                                className={" block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
+                                type="text"
+                                id={"lastNameInput"}
+                            ></input>
+                        </div>
                     </div>
-                    <br className={"block md:hidden"}/>
-                    <div>
-                        <label className= {" text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>* Last Name</label>
-                        <input className={" block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"} type="text"></input>
+                    <div className={"flex flex-col mt-3 w-full"}>
+                        <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>*
+                            Password</label>
+                        <input
+                            className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
+                            type="password"></input>
                     </div>
-                </div>
-                <div className= {"flex flex-col mt-3 w-full"}>
-                    <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>* Password</label>
-                    <input className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"} type="password"></input>
-                </div>
-                <div className= {"flex flex-col mt-3 w-full"}>
-                    <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>* Confirm Password</label>
-                    <input className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"} type="password"></input>
-                </div>
-                <div class={"flex flex-col mt-3 w-full"}>
-                    <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>* Email</label>
-                    <input className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"} type="text"></input>
-                </div>
-                <div className={ "my-6 text-white flex bg-[#509E82] mx-auto h-full justify-center w-full text-lg p-2 items-center font-medium cursor-pointer rounded-lg hover:bg-[#4E8E77]"}>
-                    <span class="register">Continue</span>
-                </div>
-                <div class={"mt-4 mb-6 text-black flex border border-2 border-[#509E82] bg-white mx-auto h-full justify-center w-full text-md p-2 items-center font-normal cursor-pointer rounded-lg hover:bg-gray-100"}>
-                    <span class="g-register">Continue with Google </span>
-                    <div className={"ml-2 "}><FcGoogle size={30}/></div>
+                    <div className={"flex flex-col mt-3 w-full"}>
+                        <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>*
+                            Confirm Password</label>
+                        <input
+                            className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
+                            type="password"></input>
+                    </div>
+                    <div className={"flex flex-col mt-3 w-full"}>
+                        <label className={"text-left block mb-2 text-xl font-medium text-gray-900 dark:text-white"}>*
+                            Email</label>
+                        <input
+                            className={"block w-full p-2 text-gray-900 border border-[#509E82] border-2 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"}
+                            type="text"></input>
+                    </div>
+                    <div
+                        className={"my-6 text-white flex bg-[#509E82] mx-auto h-full justify-center w-full text-lg p-2 items-center font-medium cursor-pointer rounded-lg hover:bg-[#4E8E77]"}>
+                        <span className="register">Continue</span>
+                    </div>
+                    <div
+                        className={"mt-4 mb-6 text-black flex border border-2 border-[#509E82] bg-white mx-auto h-full justify-center w-full text-md p-2 items-center font-normal cursor-pointer rounded-lg hover:bg-gray-100"}>
+                        <span className="g-register">Continue with Google </span>
+                        <div className={"ml-2 "}><FcGoogle size={30}/></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
